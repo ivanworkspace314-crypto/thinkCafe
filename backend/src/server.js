bd5313f945c0843ce57040a4d76e8d0be49fa657
navilo0314 <navilo314@gmail.com>
2025-11-16 15:58:29 +0800

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,9 +30,9 @@ app.use((req,res,next)=>{
 app.use(rateLimiter);
 app.use('/api/notes',notesRoutes); 
 if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,'../frontend/dist')))
+  app.use(express.static(path.join(__dirname,'../../frontend/dist')))
 app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,'../frontend/dist','index.html'))
+  res.sendFile(path.join(__dirname,'../../frontend/dist','index.html'))
 })
 }
 connectDB().then(()=>{
