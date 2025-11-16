9137bc3593d1c8d0824206bec8513264f77b2151
navilo0314 <navilo314@gmail.com>
2025-11-16 15:47:01 +0800

    prepare for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router";
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 import toast from "react-hot-toast";
